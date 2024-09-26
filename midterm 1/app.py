e100b058a801f93802d4a8851476457c65ec2569
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2024-09-26 08:31:55 +0000

    modified:   midterm 1/app.py 	new file:   model..pkl

--- midterm 1/app.py
+++ midterm 1/app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-with open('model.pkl', 'rb') as file:
+with open('model..pkl', 'rb') as file:
     model = pickle.load(file)
 
 @app.route('/')
