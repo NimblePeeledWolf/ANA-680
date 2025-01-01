a28c5de41bc1bfe3c1a4dc9ac7198936d92baf69
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2025-01-01 21:00:43 +0000

    modified:   midterm 1/app.py 	modified:   midterm 1/untitled.ipynb

--- midterm 1/app.py
+++ midterm 1/app.py
@@ -15,7 +15,7 @@ y = data['race/ethnicity']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # Load the trained model
-with open('./workspaces/ANA-680/midterm 1/model.pkl', 'rb') as file:
+with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 @app.route('/')
