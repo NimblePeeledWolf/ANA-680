55dbd3cd78e514b4a5f9da33ef68a4ca8209631c
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2025-01-06 03:25:08 +0000

    modified:   app.py 	new file:   model2.pkl 	modified:   untitled.ipynb

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import os
-print("Current Working Directory:", os.getcwd())
-os.chdir('/workspaces/ANA-680/midterm 1/')
+
 
 app = Flask(__name__)
 
@@ -18,7 +16,7 @@ y = data['race/ethnicity']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # Load the trained model
-with open('model.pkl', 'rb') as file:
+with open('model2.pkl', 'rb') as file:
     model = pickle.load(file)
 
 @app.route('/')
